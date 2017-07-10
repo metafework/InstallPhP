a091cf2fad8ca45fe62db932004dcb9af24ff42e
metafework <metmiku.afework@gmail.com>
2017-07-10 02:25:45 +0000

    auto yes

--- intall.sh
+++ intall.sh
@@ -1,4 +1,4 @@
-sudo apt install apache2
-sudo apt install php
-sudo apt install libapache2-mod-php
+sudo apt install apache2 -y	
+sudo apt install php -y
+sudo apt install libapache2-mod-php -y
 sudo cp test.php /var/www/html
